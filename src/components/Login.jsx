8a189d29064b6cca6d00d884cf127c8842092590
marreropd <pablomarrerofernandez1@gmail.com>
2022-06-15 18:12:43 -0300

    validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 function Login() {
+  const sumbitHandler = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+    if (email === "" || password === "") {
+      console.log("los campos no pueden estar vacios");
+    }
+  };
   return (
     <>
       <h2>Ingreso</h2>
-      <form>
+      <form onSubmit={sumbitHandler}>
         <label>
           <span>Correo Electronico</span>
           <br />
